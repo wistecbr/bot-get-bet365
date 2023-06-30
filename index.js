36387ae55dea6b37fea8d544d1ba323dddc6099c
Wisley Alves <wisley7lagoas@yahoo.com.br>
2023-06-30 13:21:42 -0300

    publish docker

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import run from './lib/bot.js'
 const timeOut = 1000 * 30
-
+// /*
 setInterval(async () => {
   console.log('>> ', new Date())
   await Promise.all([
     run('euro', 'Euro Cup'),
-    // run('copa', 'Copa do Mundo'),
-    // run('super', 'Super Liga Sul-Americana'),
-    // run('premier', 'Premier League')
+    run('copa', 'Copa do Mundo'),
+    run('super', 'Super Liga Sul-Americana'),
+    run('premier', 'Premier League')
   ])
 }, timeOut)
+//*/
 
 // run('euro', 'Euro Cup')
