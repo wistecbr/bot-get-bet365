5861da705942f0b0f8584c01a90569f4c54cb6c7
Wisley Alves <wisley7lagoas@yahoo.com.br>
2023-06-30 10:04:45 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# bot-get-bet365
---
+++ index.js
@@ -0,0 +1,12 @@
+import run from './lib/bot.js'
+const timeOut = 1000 * 30
+
+setInterval(async () => {
+  console.log('>> ', new Date())
+  await Promise.all([
+    run('euro', 'Euro Cup'),
+    run('copa', 'Copa do Mundo'),
+    run('super', 'Super Liga Sul-Americana'),
+    run('premier', 'Premier League')
+  ])
+}, timeOut)
