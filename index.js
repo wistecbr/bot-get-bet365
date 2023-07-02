ea617f4e06a5848e3492bc1b765b4573aa149076
Wisley Alves <wisley7lagoas@yahoo.com.br>
2023-07-02 16:57:20 -0300

    update index

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import run from './lib/bot.js'
-import { getPage } from './lib/bot.js'
+import run, { getPage } from './lib/bot.js'
 const timeOut = 1000
 
 const delay = (time) => {
@@ -9,10 +8,13 @@ const delay = (time) => {
   })
 }
 const runAll = async () => {
-  const page = await getPage()
+  let i = 0
+  // let page
+  // let browser
   while (true) {
+    const { page, browser } = await getPage()
     const initTime = new Date()
-    console.log('>> ', initTime)
+    console.log('>> ', initTime, ' ', i)
     // await Promise.all([
     await run('euro', 'Euro Cup', page)
     // await delay()
@@ -22,11 +24,16 @@ const runAll = async () => {
     // await delay()
     await run('premier', 'Premier League', page)
     // ])
+    await browser.close()
     const endTime = new Date()
     const difTime = endTime - initTime
     const timeDelay = (30000 - difTime) / 1000
     console.log('Took: ', difTime, ' ', timeDelay)
+    i += 1
+
     await delay(timeDelay > 0 ? timeDelay : 1)
+
+
   }
 }
 
