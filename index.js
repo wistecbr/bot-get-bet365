1437e7cf382fa1f3bc07bf4b3ed23b445ed44595
Wisley Alves <wisley7lagoas@yahoo.com.br>
2023-07-02 12:20:28 -0300

    enable concurence

--- index.js
+++ index.js
@@ -11,17 +11,20 @@ const runAll = async () => {
   while (true) {
     const initTime = new Date()
     console.log('>> ', initTime)
-    await run('euro', 'Euro Cup')
-    // await delay()
-    await run('copa', 'Copa do Mundo')
-    // await delay()
-    await run('super', 'Super Liga Sul-Americana')
-    // await delay()
-    await run('premier', 'Premier League')
+    await Promise.all([
+      run('euro', 'Euro Cup'),
+      // await delay()
+      run('copa', 'Copa do Mundo'),
+      // await delay()
+      run('super', 'Super Liga Sul-Americana'),
+      // await delay()
+      run('premier', 'Premier League')
+    ])
     const endTime = new Date()
     const difTime = endTime - initTime
-    console.log('Took: ', difTime)
-    await delay(10)
+    const timeDelay = (30000 - difTime) / 1000
+    console.log('Took: ', difTime, ' ', timeDelay)
+    await delay(timeDelay > 0 ? timeDelay : 1)
   }
 }
 
