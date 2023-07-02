2935dc368f17e5c418be9ae5bbf0efde1c07a956
Wisley Alves <wisley7lagoas@yahoo.com.br>
2023-07-02 11:58:08 -0300

    update index

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 import run from './lib/bot.js'
-const timeOut = 1000 * 30
+const timeOut = 1000
+
 const delay = (time) => {
   return new Promise(resolve => {
-    console.log(`Await ${time || timeOut}s`)
-    setTimeout(() => resolve(true), time || timeOut)
+    console.log(`Await ${(time || 1) * timeOut}ms`)
+    setTimeout(() => resolve(true), (time || 1) * timeOut)
   })
 }
-// /*
-setInterval(async () => {
-  console.log('>> ', new Date())
-  // await Promise.all([
-  await run('euro', 'Euro Cup')
-  await delay()
-  await run('copa', 'Copa do Mundo')
-  await delay()
-  await run('super', 'Super Liga Sul-Americana')
-  await delay()
-  await run('premier', 'Premier League')
-  // ])
-}, timeOut)
-//*/
+const runAll = async () => {
+  while (true) {
+    const initTime = new Date()
+    console.log('>> ', initTime)
+    await run('euro', 'Euro Cup')
+    // await delay()
+    await run('copa', 'Copa do Mundo')
+    // await delay()
+    await run('super', 'Super Liga Sul-Americana')
+    // await delay()
+    await run('premier', 'Premier League')
+    const endTime = new Date()
+    const difTime = endTime - initTime
+    console.log('Took: ', difTime)
+    await delay(10)
+  }
+}
 
-// run('euro', 'Euro Cup')
+runAll()
