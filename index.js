66e5c6b5bf3e43cabae391cd3fe1e4fd5eea3c26
Wisley Alves <wisley7lagoas@yahoo.com.br>
2023-07-02 10:26:09 -0300

    update dir test

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 import run from './lib/bot.js'
 const timeOut = 1000 * 30
+const delay = (time) => {
+  return new Promise(resolve => {
+    console.log(`Await ${time || timeOut}s`)
+    setTimeout(() => resolve(true), time || timeOut)
+  })
+}
 // /*
 setInterval(async () => {
   console.log('>> ', new Date())
   // await Promise.all([
   await run('euro', 'Euro Cup')
+  await delay()
   await run('copa', 'Copa do Mundo')
+  await delay()
   await run('super', 'Super Liga Sul-Americana')
+  await delay()
   await run('premier', 'Premier League')
   // ])
 }, timeOut)
