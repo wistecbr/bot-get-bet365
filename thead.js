65fdcff9fb6aaccffa271e5d44142affbe5cb81c
Wisley Alves <wisley7lagoas@yahoo.com.br>
2023-07-04 09:17:07 -0300

    init thread

---
+++ thead.js
@@ -0,0 +1,35 @@
+import { Worker, workerData } from 'worker_threads'
+
+
+const __dirname = !process.env.TEST ? '/home/wisley/projetos/bot-get-bet365/' : process.cwd()
+
+const startWorker = (path, campeonato, nomeCampeonato, cb) => {
+  const worker = new Worker(path, { workerData: { campeonato, nomeCampeonato } })
+  worker.on('message', (msg) => {
+    cb(null, msg)
+  })
+  worker.on('error', cb)
+  worker.on('exit', (code) => {
+    if (code != 0)
+      console.error(new Error(`Worker finalizado com exit code = ${code}`))
+  })
+  return worker
+}
+
+console.log("Thread principal")
+
+const callbackWorker = (err, result) => {
+  if (err) return console.error(err)
+  // console.log("** COMPUTAÇÃO PESADA FINALIZADA **")
+  console.log(`>(${result.campeonato}) Duração = ${(result.end - result.start) / 1000} segundos`)
+}
+
+// Inicia o worker em outra thread
+startWorker(__dirname + '/lib/worker.js', 'premier', 'Premier League', callbackWorker)
+startWorker(__dirname + '/lib/worker.js', 'euro', 'Euro Cup', callbackWorker)
+startWorker(__dirname + '/lib/worker.js', 'copa', 'Copa do Mundo', callbackWorker)
+startWorker(__dirname + '/lib/worker.js', 'super', 'Super Liga Sul-Americana', callbackWorker)
+
+
+
+
